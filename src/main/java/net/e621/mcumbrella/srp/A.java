e21376de0bdbf79b75ca8c8cd9221494dab15a61
MCUmbrella <mcumbrella@qq.com>
2020-10-08 22:09:05 +0800

    INT 0000

---
+++ src/main/java/net/e621/mcumbrella/srp/A.java
@@ -0,0 +1,10 @@
+package net.e621.mcumbrella.srp;
+import javax.mail.*;
+import java.util.Properties;
+public class A {
+    public static void main(String[] args) //throws MessagingException
+    {
+        System.out.println("[i] Started.");
+        System.exit(0);
+    }
+}
